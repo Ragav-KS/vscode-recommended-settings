892d1fa7e9b09ded10bca905b01d6df3758a11ae
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2024-12-09 11:23:29 +0530

    feat: load settings from file

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,13 @@
-import { window, commands, type ExtensionContext } from "vscode";
+import path from "path";
+import {
+  commands,
+  ConfigurationTarget,
+  Uri,
+  window,
+  workspace,
+  type ExtensionContext,
+  type FileStat,
+} from "vscode";
 
 export function activate(context: ExtensionContext) {
   console.log("Activating extension");
@@ -6,7 +15,46 @@ export function activate(context: ExtensionContext) {
   const disposable = commands.registerCommand(
     "recommended-settings.load-recommended-settings",
     () => {
-      window.showInformationMessage("Hello World from Recommended Settings!");
+      const filename = "recommended-settings.json";
+
+      workspace.workspaceFolders?.forEach(async (workspaceFolder) => {
+        const folderPath = workspaceFolder.uri.fsPath;
+
+        const filePath = path.join(path.join(folderPath, ".vscode"), filename);
+        const fileUri = Uri.file(filePath);
+
+        let fileExists: FileStat | undefined = undefined;
+        try {
+          fileExists = await workspace.fs.stat(fileUri);
+        } catch (err) {
+          console.log("Recommended settings file not found.");
+        }
+
+        if (fileExists) {
+          console.log("Found recommended-settings.json. Loading settings");
+
+          const recommendedSettingsJson = await workspace
+            .openTextDocument(fileUri)
+            .then(
+              (document) =>
+                JSON.parse(document.getText()) as Record<string, any>
+            );
+
+          Object.entries(recommendedSettingsJson).forEach(([key, value]) => {
+            workspace
+              .getConfiguration()
+              .update(key, value, ConfigurationTarget.Global);
+          });
+
+          window.showInformationMessage(
+            "Loaded workspace recommended settings to Global settings."
+          );
+        } else {
+          window.showErrorMessage(
+            "Recommended settings file not found in workspace."
+          );
+        }
+      });
     }
   );
 
