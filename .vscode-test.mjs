7cfba40528487e239fc87e8e5427db20ff1107a3
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2024-12-25 23:51:32 +0530

    test: configure integration test for invalid workspace folder.

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -5,5 +5,10 @@ export default defineConfig([
     label: "Valid Single Folder Workspace",
     files: "out/test/integ/validWorkspace/**/*.test.js",
     workspaceFolder: "./sampleWorkspaces/singleFolderWorkspace",
+  }, 
+  {
+    label: "Invalid Workspace",
+    files: "out/test/integ/invalidWorkspace/**/*.test.js",
+    workspaceFolder: "./sampleWorkspaces/invalidWorkspace",
   },
 ]);
