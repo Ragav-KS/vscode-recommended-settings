f392c4d67820dbff2c769e20d620e83ec6bb7c5d
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2024-12-25 23:52:23 +0530

    test: command should fail if recommended settings file is not found

---
+++ src/test/integ/invalidWorkspace/load-recommended-settings.test.ts
@@ -0,0 +1,38 @@
+import { ok } from "assert";
+import Sinon, { type SinonSpy } from "sinon";
+import { commands, window } from "vscode";
+
+suite(
+  "'Load Project Recommended Settings' command functions as expected",
+  () => {
+    let errorMessageSpy: SinonSpy;
+
+    suiteSetup(() => {
+      errorMessageSpy = Sinon.spy(window, "showErrorMessage");
+    });
+
+    test("command should be registered", async () => {
+      const registeredCommands = await commands.getCommands();
+
+      ok(
+        registeredCommands.includes(
+          "recommended-settings.load-recommended-settings"
+        )
+      );
+    });
+
+    test("Command should fail if recommended settings file is not found", async () => {
+      // trigger
+      await commands.executeCommand(
+        "recommended-settings.load-recommended-settings"
+      );
+
+      // assert
+      ok(
+        errorMessageSpy.calledOnceWith(
+          "Recommended settings file not found in workspace."
+        )
+      );
+    });
+  }
+);
