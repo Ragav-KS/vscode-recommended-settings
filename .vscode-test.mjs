f05bf3e0532bd42e1b276e367a3be859742a9c2c
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2024-12-26 01:45:14 +0530

    test: configure unit tests

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from "@vscode/test-cli";
 
 export default defineConfig([
   {
+    label: "Unit tests",
+    files: ["out/**/*.spec.js"],
+  },
+  {
     label: "Valid Single Folder Workspace",
     files: "out/test/integ/validWorkspace/**/*.test.js",
     workspaceFolder: "./sampleWorkspaces/singleFolderWorkspace",
-  }, 
+  },
   {
     label: "Invalid Workspace",
     files: "out/test/integ/invalidWorkspace/**/*.test.js",
