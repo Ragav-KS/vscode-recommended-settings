ce62322cd831dee9437fdfc3c774febbac7c64f1
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2024-12-29 01:30:26 +0530

    refactor: fix mistake

--- src/test/integ/invalidWorkspace/activation.test.ts
+++ src/test/integ/invalidWorkspace/activation.test.ts
@@ -2,7 +2,7 @@ import { ok } from "assert";
 import { extensions } from "vscode";
 
 suite("Extension activation is as expected", () => {
-  test("Extension should activate", async () => {
+  test("Extension should not activate automatically", async () => {
     const extension = extensions.getExtension("ragavks.recommended-settings");
 
     ok(extension, "Extension found");
