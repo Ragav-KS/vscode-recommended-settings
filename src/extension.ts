afc9a988dffc31cf725840ef8bd5e38ef73f3b26
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2025-01-01 00:58:59 +0530

    chore: show notification on extension activate

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,11 @@
 import { commands, type ExtensionContext } from "vscode";
 import { loadRecommendedSettings } from "./utils/loadRecommendedSettings";
+import { showNotificationIfFileExists } from "./utils/showNotificationIfFileExists";
 
 export function activate(context: ExtensionContext) {
   console.log("Activating extension");
+
+  showNotificationIfFileExists();
 
   const disposable = commands.registerCommand(
     "recommended-settings.load-recommended-settings",
