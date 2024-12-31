a1308c0ea8e5b11e653c5179fb4928f8ba0ee993
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2025-01-01 00:58:31 +0530

    chore: function to show notification if file exists

---
+++ src/utils/showNotificationIfFileExists.ts
@@ -0,0 +1,27 @@
+import { window, workspace } from "vscode";
+import { getUriIfFileExists } from "./getUriIfFileExists";
+import { loadSettingsFromFile } from "./loadSettingsFromFile";
+
+export async function showNotificationIfFileExists() {
+  if (!workspace.workspaceFolders || workspace.workspaceFolders.length !== 1) {
+    return;
+  }
+
+  const workspaceFolder = workspace.workspaceFolders[0];
+
+  const fileUri = await getUriIfFileExists(workspaceFolder);
+
+  if (!fileUri) {
+    return;
+  }
+
+  const selection = await window.showInformationMessage(
+    "Workspace has recommended some settings. Do you want to load them?",
+    "Yes",
+    "No"
+  );
+
+  if (selection === "Yes") {
+    await loadSettingsFromFile(fileUri);
+  }
+}
